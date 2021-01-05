62bc7fe79ff76173f6906f5830a0b57d36551a12
Belyaev <sergei.beleaev@gmail.com>
2021-01-05 14:59:46 +0300

    added: xmlParser.js

---
+++ codewars/xmlParser.js
@@ -0,0 +1,18 @@
+function catalog(s, article) {
+
+  function getParam(str, name) {
+    let regStart = new RegExp(`<${name}>`, 'i')
+    let regEnd = new RegExp(`<\/${name}>`, 'i')
+    return str.slice(str.match(regStart).index + name.length + 2, str.match(regEnd).index)
+  }
+
+  const arr = s.split('\n\n')
+  const result = arr.filter(str => str.includes(article))
+
+  return result[0] ?
+    result.reduce((acc, val) =>
+      acc + (acc ? '\r\n' : '')
+      + `${getParam(val, 'name')} > prx: $${getParam(val, 'prx')} qty: ${getParam(val, 'qty')}`
+      , '')
+    : 'Nothing'
+}
